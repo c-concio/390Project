f31051c92a2679552ab597b9d1ecc77a161a5646
c-concio <chris-768@hotmail.com>
2019-02-24 19:39:04 -0500

    Created an employee activity showing completed tasks and assigned tasks

--- app/src/main/java/com/example/a390project/Fragments/EmployeeFragment.java
+++ app/src/main/java/com/example/a390project/Fragments/EmployeeFragment.java
@@ -1,5 +1,6 @@
 package com.example.a390project.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -7,9 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.a390project.DummyDatabase;
+import com.example.a390project.EmployeeActivity;
 import com.example.a390project.Model.Employee;
 import com.example.a390project.ListViewAdapters.EmployeeListViewAdapter;
 import com.example.a390project.R;
@@ -39,12 +42,27 @@ public class EmployeeFragment extends Fragment {
         employees = new DummyDatabase().generateDummyEmployees(25);
 
         listViewAdapter();
+
+
     }
 
     // setup the components in the layout
     private void setupUI(){
         // find the views
         employeeListView = view.findViewById(R.id.employeesListView);
+
+        // setup what happens when a list view item is clicked
+        employeeListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // go to the employee's activity
+                Intent intent = new Intent(view.getContext(), EmployeeActivity.class);
+                intent.putExtra("employeeID", employees.get(position).getEmployeeID());
+                startActivity(intent);
+            }
+        });
     }
 
     // setup the adapter for the employees list view
