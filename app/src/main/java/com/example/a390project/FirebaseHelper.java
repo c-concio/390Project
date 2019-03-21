67465d9580ceabc630f8eb137b924cc1871907ee
c-concio <chris-768@hotmail.com>
2019-03-20 23:03:38 -0400

    Added comment section for the pre-painting task

--- app/src/main/java/com/example/a390project/FirebaseHelper.java
+++ app/src/main/java/com/example/a390project/FirebaseHelper.java
@@ -136,7 +136,7 @@ public class FirebaseHelper {
                     String machineLastEmployee =  ds.child("machineLastEmployee").getValue(String.class);
                     String machineType = ds.child("machineType").getValue(String.class);
                     boolean machineStatus = ds.child("machineStatus").getValue(boolean.class);
-                    float temperature = ds.child("Temperature").getValue(float.class);
+                    float temperature = ds.child("temperature").getValue(float.class);
 
                     if (machineType.equals("Oven")) {
                         long machineStatusTimeOff = ds.child("machineStatusTimeOff").getValue(long.class);
@@ -145,7 +145,7 @@ public class FirebaseHelper {
                                 machineStatusTimeOff, machineStatusTimeOn));
                     }
                     else if (machineType.equals("PaintBooth")) {
-                        float humidity = ds.child("Humidity").getValue(float.class);
+                        float humidity = ds.child("humidity").getValue(float.class);
                         machines.add(new Paintbooth(machineTitle, machineLastEmployee, machineStatus, temperature, machineType,
                                 humidity));
                     }
@@ -1057,5 +1057,10 @@ public class FirebaseHelper {
         ViewGroup.LayoutParams params = listView.getLayoutParams();
         params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
         listView.setLayoutParams(params);
+    }
+
+    // function that saves a comment to the specified task
+    private void postComment(Activity activity, String taskID, EmployeeComment newComment){
+
     }
 }
--- app/src/main/java/com/example/a390project/TaskPrePaintingActivity.java
+++ app/src/main/java/com/example/a390project/TaskPrePaintingActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TaskPrePaintingActivity extends AppCompatActivity {
 
@@ -13,6 +17,8 @@ public class TaskPrePaintingActivity extends AppCompatActivity {
     private ListView prepaintTasksListView;
     private FirebaseHelper firebaseHelper;
     private String taskId;
+    private Button postCommentButton;
+    private EditText newEmployeeCommentEdtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +40,30 @@ public class TaskPrePaintingActivity extends AppCompatActivity {
     private void setupUI(){
         prepaintTasksListView = findViewById(R.id.prepaintTaskListView);
         mDescription = findViewById(R.id.prepaint_task_description);
+
         mDescription.setText(getIntent().getStringExtra("prepaintingDescription"));
         taskId = getIntent().getStringExtra("prepaintingTaskID");
         firebaseHelper = new FirebaseHelper();
+
+        // comment function
+        postCommentButton = findViewById(R.id.postCommentButton);
+        newEmployeeCommentEdtText = findViewById(R.id.newEmployeeCommentEditText);
+
+        postCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTextValue = newEmployeeCommentEdtText.getText().toString();
+
+                // check if the editText field is empty, if it is empty output error
+                if (editTextValue.isEmpty()){
+                    newEmployeeCommentEdtText.setError("Field is empty");
+                }
+                // else save comment into firebase
+
+
+
+            }
+        });
     }
 
     //custom heading and back button
